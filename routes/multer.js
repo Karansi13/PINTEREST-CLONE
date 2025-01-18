663d8702d131506a10b1d5104866b4901cdac3d2
karan <ks888570@gmail.com>
2025-01-18 20:15:48 +0530

    changes

--- routes/multer.js
+++ routes/multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer")
 const {v4: uuidv4} = require("uuid")
 const path = require("path")
-
+// for storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, './public/images/uploads')
